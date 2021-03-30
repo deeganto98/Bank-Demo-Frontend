4d42dbeb020ef8ea20c2d13f26d312680aef2864
deeganto98 <50057038+deeganto98@users.noreply.github.com>
2021-03-30 23:46:05 +0530

    component-addition-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeLoanComponent } from './home-loan/home-loan.component';
+import { PaymentComponent } from './payment/payment.component';
+import { VehicleLoanComponent } from './vehicle-loan/vehicle-loan.component';
+import { WalletComponent } from './wallet/wallet.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    DashboardComponent,
+    HomeLoanComponent,
+    PaymentComponent,
+    VehicleLoanComponent,
+    WalletComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
